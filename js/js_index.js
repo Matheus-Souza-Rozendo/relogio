505faf5ccb921e6aa76d754b50a24632af151126
Matheus-Souza-Rozendo <matheusrozendo234674@gmail.com>
2021-09-05 14:57:22 -0300

    Update js_index.js

--- js/js_index.js
+++ js/js_index.js
@@ -6,13 +6,13 @@ var ver=true;
 
 function escolheTema(){
     if((hora.hora<6)||(hora.hora>18)){
-        document.getElementById("artigo").style.backgroundImage="url(/imagens/noite.jpg)";
+        document.getElementById("artigo").style.backgroundImage="url(imagens/noite.jpg)";
         hora.mudaCor("#FFFFFF","hora");
         d.mudaCor("#FFFFFF","calendar");
         document.getElementById("footer").style.backgroundColor="rgb(0,0,0)";
         document.getElementById("footer").style.color="rgb(255,255,255)";
     }else{
-        document.getElementById("artigo").style.backgroundImage="url(/imagens/dia.jpg)";
+        document.getElementById("artigo").style.backgroundImage="url(imagens/dia.jpg)";
     }
 }
 
