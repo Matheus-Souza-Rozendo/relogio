b005e30d03a1a534f9931e1db67248f322e25c96
Matheus-Souza-Rozendo <matheusrozendo234674@gmail.com>
2021-09-05 14:57:53 -0300

    Update js_cronometro.js

--- js/js_cronometro.js
+++ js/js_cronometro.js
@@ -3,12 +3,12 @@ var hora= data.getHours();
 
 function escolheTema(){
     if((hora<6)||(hora>18)){
-        document.getElementById("artigo").style.backgroundImage="url(/imagens/noite.jpg)";
+        document.getElementById("artigo").style.backgroundImage="url(imagens/noite.jpg)";
         mudaCor("#FFFFFF");
         document.getElementById("footer").style.backgroundColor="rgb(0,0,0)";
         document.getElementById("footer").style.color="rgb(255,255,255)";
     }else{
-        document.getElementById("artigo").style.backgroundImage="url(/imagens/dia.jpg)";
+        document.getElementById("artigo").style.backgroundImage="url(imagens/dia.jpg)";
     }
 }
 
